035aa968141b3bb9faf7ee2181fff2eecebcfe6a
Colin Stephen <code@c0l.in>
2014-09-07 15:01:08 -0700

    include basic blocking ajax

---
+++ labs/week4/js/utils.js
@@ -0,0 +1,61 @@
+var fakeSlowNetwork;
+
+(function() {
+  var lsKey = 'fake-slow-network';
+  var networkFakeDiv = document.querySelector('.network-fake');
+  var checkbox = networkFakeDiv.querySelector('input');
+
+  fakeSlowNetwork = Number(localStorage.getItem(lsKey)) || 0;
+
+  networkFakeDiv.style.display = 'block';
+  checkbox.checked = !!fakeSlowNetwork;
+
+  checkbox.addEventListener('change', function() {
+    localStorage.setItem(lsKey, Number(checkbox.checked));
+    location.reload();
+  });
+}());
+
+function getSync(url) {
+  var startTime = Date.now();
+  var waitTime = 3000 * Math.random() * fakeSlowNetwork;
+
+  var req = new XMLHttpRequest();
+  req.open('get', url, false);
+  req.send();
+
+  while (waitTime > Date.now() - startTime);
+
+  if (req.status == 200) {
+    return req.response;
+  }
+  else {
+    throw Error(req.statusText || "Request failed");
+  }
+}
+
+function getJsonSync(url) {
+  return JSON.parse(getSync(url));
+}
+
+function getJsonCallback(url, callback) {
+  getJson(url).then(function(response) {
+    callback(undefined, response);
+  }, function(err) {
+    callback(err);
+  });
+}
+
+var storyDiv = document.querySelector('.story');
+
+function addHtmlToPage(content) {
+  var div = document.createElement('div');
+  div.innerHTML = content;
+  storyDiv.appendChild(div);
+}
+
+function addTextToPage(content) {
+  var p = document.createElement('p');
+  p.textContent = content;
+  storyDiv.appendChild(p);
+}
