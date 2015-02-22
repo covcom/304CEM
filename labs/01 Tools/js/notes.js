c3e4a00351dcb4f98393c0239fe9bb5ba6726573
marktyers <marktyers@gmail.com>
2015-02-22 14:48:25 +0000

    Removed LocalStorage removed all references to LocalStorage.

--- labs/01 Tools/js/notes.js
+++ labs/01 Tools/js/notes.js
@@ -2,27 +2,20 @@
 var notes = new Array();
 
 function addItem() {
-	console.log('addNote');
 	textbox = document.getElementById('item');
 	var itemText = textbox.value;
 	textbox.value = '';
 	textbox.focus();
-	console.log(itemText);
 	var newItem = {title: itemText, quantity: 1};
 	notes.push(newItem);
-	console.log(notes);
-	displayList();
-	saveList();
 }
 
 function displayList() {
-	console.log('displayList');
 	var table = document.getElementById('list');
 	table.innerHTML = '';
 	for (var i = 0; i<notes.length; i++) {
 		var node = undefined;
 		var note = notes[i];
-		console.log(note);
 		var node = document.createElement('tr');
 		var html = '<td>'+note.title+'</td><td>'+note.quantity+'</td><td><a href="#" onClick="deleteIndex('+i+')">delete</td>';
 	    node.innerHTML = html;
@@ -31,23 +24,7 @@ function displayList() {
 }
 
 function deleteIndex(i) {
-	console.log('deleteIndex');
-	console.log(i);
 	notes.splice(i, 1);
-	displayList();
-	saveList();
-}
-
-function saveList() {
-	localStorage.notes = JSON.stringify(notes);
-}
-
-function loadList() {
-	console.log('loadList');
-	if (localStorage.notes) {
-		notes = JSON.parse(localStorage.notes);
-		displayList();
-	}
 }
 
 button = document.getElementById('add')
