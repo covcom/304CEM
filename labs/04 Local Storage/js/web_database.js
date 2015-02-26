2e9a7f2a750e2865c9fde270e1a467a8aecbf9a6
marktyers <marktyers@gmail.com>
2015-02-26 22:05:29 +0000

    Added web database activity

---
+++ labs/04 Local Storage/js/web_database.js
@@ -0,0 +1,11 @@
+
+var db = openDatabase('todo', '1.0', 'simple todo list', 2 * 1024 * 1024);
+
+document.querySelector('button').onclick = function() {
+	db.transaction(function (tx) {
+		var newItem = document.querySelector('input').value;
+		tx.executeSql('CREATE TABLE IF NOT EXISTS items (item VARCHAR(45))');
+		tx.executeSql('INSERT INTO items (item) VALUES ("Mark Tyers")');
+	});
+};
+
