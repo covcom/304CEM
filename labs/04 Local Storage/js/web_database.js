7ee402827f885164bdb570139d9d15a9c73bca1a
marktyers <marktyers@gmail.com>
2015-02-26 22:19:42 +0000

    fixed bug in web database

--- labs/04 Local Storage/js/web_database.js
+++ labs/04 Local Storage/js/web_database.js
@@ -5,7 +5,6 @@ document.querySelector('button').onclick = function() {
 	db.transaction(function (tx) {
 		var newItem = document.querySelector('input').value;
 		tx.executeSql('CREATE TABLE IF NOT EXISTS items (item VARCHAR(45))');
-		tx.executeSql('INSERT INTO items (item) VALUES ("Mark Tyers")');
+		tx.executeSql('INSERT INTO items (item) VALUES ("'+newItem+'")');
 	});
 };
-
