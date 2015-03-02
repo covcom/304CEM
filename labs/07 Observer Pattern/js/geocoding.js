1185837bd3262ea5295ee84be667781ac9f576ff
marktyers <marktyers@gmail.com>
2015-03-02 10:40:02 +0000

    working RxJS example using button

---
+++ labs/07 Observer Pattern/js/geocoding.js
@@ -0,0 +1,20 @@
+console.log('page loaded');
+
+/*
+var input = document.querySelector('input');
+
+var keyups = Rx.Observable.fromEvent(input, 'keyup')
+	.map(function(e) {
+		return e.target.value;
+	})
+	.filter(function(text) {
+		return text.length > 2;
+	})
+	.forEach(function(e) {
+		console.log(e);
+	});
+
+function search(term) {
+	return term;
+}
+*/
---
+++ labs/07 Observer Pattern/js/observable_button.js
@@ -0,0 +1,43 @@
+/*
+ * Simple example
+ * we create an observable object to handle keypresses.
+ * Operators http://reactivex.io/documentation/operators.html
+ * .skip	suppress the first n items emitted by an Observable
+ * .take	emit only the first n items emitted by an Observable
+ * .map		apply a function to each element.
+ */
+
+function CreateObservable(element, eventType) {
+	return Rx.Observable.create(function(observer) {
+		function eventHandler (eventObj) {
+			observer.onNext(eventObj);
+		}
+		element.addEventListener(eventType, eventHandler);
+		return function() {
+			element.removeEventListener(eventType, eventHandler);
+		};
+	});
+}
+
+var btn = document.getElementById('button');
+
+var observable = CreateObservable(btn, 'click')
+	.skip(2)
+	.take(2)
+	.map(function(evt){
+		return "button was clicked";
+	});
+
+var observer = Rx.Observer.create(
+	function(evt) {
+		console.log(evt);
+	},
+	function(err) {
+		console.log('error');
+	},
+	function() {
+		console.log('done');
+	}
+);
+
+observable.subscribe(observer);
