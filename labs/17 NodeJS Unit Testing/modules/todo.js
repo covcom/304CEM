09b98f3f5d76e1a265a5d28013d93b73e82395ea
marktyers <marktyers@gmail.com>
2015-03-30 14:48:05 +0000

    created sync tests

--- labs/17 NodeJS Unit Testing/modules/todo.js
+++ labs/17 NodeJS Unit Testing/modules/todo.js
@@ -2,20 +2,29 @@
 storage = require('node-persist');
 storage.initSync();
 
-exports.add = function(title) {
-	storage.setItem(title, title);
+// add a new item to the todo list
+exports.add = function(title, qty) {
+	storage.setItem(title, {title: title, qty: qty});
 	return true;
 };
 
+// calculates and returns the number of items in the list
 exports.count = function() {
-	return storage.values().length;
+	return storage.length();
 };
 
+// empties the list
 exports.clear = function() {
 	storage.clearSync();
 	return true;
 };
 
+// returns an array containing all todo items
 exports.getAll = function() {
 	return storage.values();
+};
+
+// removes the item with the corresponding key
+exports.remove = function(item) {
+	storage.removeItemSync(item);
 };
--- labs/17 NodeJS Unit Testing/spec/todo-spec.js
+++ labs/17 NodeJS Unit Testing/spec/todo-spec.js
@@ -9,15 +9,28 @@ describe("ToDo List", function () {
 	});
 	
 	it('should add a new item', function () {
-		list.add('milk');
-		list.add('butter');
-		list.add('cheese');
-		list.add('bread');
+		list.add('milk', 1);
+		list.add('butter', 2);
+		list.add('cheese', 3);
+		list.add('bread', 4);
 		expect(list.count()).toBe(4);
 	});
 	
-	it('second item should be "butter"', function() {
-		var items = list.getAll();
-		expect(list.getAll()[1]).toBe('butter');
+	it('second item should be "butter", qty 2', function() {
+		expect(list.getAll()[1].title).toBe('butter');
+		expect(list.getAll()[1].qty).toBe(2);
+	});
+	
+	it('should be able to remove butter', function() {
+		list.remove('butter');
+		expect(list.count()).toBe(3);
+		expect(list.getAll()[1].title).toBe('cheese');
+	});
+	
+	it('should be able to cope with an invalid key', function() {
+		//debugger;
+		list.remove('jam');
+		expect(list.count()).toBe(3);
+		expect(list.getAll()[1].title).toBe('cheese');
 	});
 });
