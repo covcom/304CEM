2b5d6c4a1d9448cb58eee7d2b666923ffe5601af
marktyers <marktyers@gmail.com>
2015-03-30 18:33:44 +0000

    added timer to main code

--- labs/17 NodeJS Unit Testing/weather.js
+++ labs/17 NodeJS Unit Testing/weather.js
@@ -1,6 +1,8 @@
 
 var weather = require('./modules/weather');
 
+console.time('getting geoData');
 weather.getGeo('London,UK', function(data) {
 	console.log(data);
+	console.timeEnd('getting geoData');
 });
