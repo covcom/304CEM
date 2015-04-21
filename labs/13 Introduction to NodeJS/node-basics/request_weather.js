fce28af4d0d225cadecf3081dc104d3277181a6e
Colin Stephen <gitlab.com@c0l.in>
2015-04-21 12:54:47 +0100

    add node worksheet

---
+++ labs/13 Introduction to NodeJS/node-basics/request_weather.js
@@ -0,0 +1,23 @@
+// import the HTTP client module
+var request = require('request');
+
+// set up the base URL
+var url = 'http://api.openweathermap.org/data/2.5/weather'
+
+// define a function to send a GET request with the city desired
+function checkWeather(city, callback) {
+    var query_string = {q: city};    
+    request.get({url: url, qs: query_string}, callback);
+}
+
+// async callback to process what comes back from the request
+function logWeather(error, response, body){
+    if (error) {
+        console.log(error);
+    } else {
+        console.log(response.statusCode, body);
+    }
+}
+
+// call the function to check the weather!
+checkWeather("coventry,uk", logWeather);
