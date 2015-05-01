581fbff339e4f27bc2299d3edca0ccb21ac1bb81
marktyers <marktyers@gmail.com>
2015-05-01 17:08:10 +0000

    started location services exercise

--- labs/09 Location Services/js/bus_stops.js
+++ labs/09 Location Services/js/bus_stops.js
@@ -1,7 +1,29 @@
 
 window.onload = function() {
 	console.log('onLoad');
+	
+	var mapProp = {
+		center:new google.maps.LatLng(51.5072, -0.01),
+		zoom:16,
+		mapTypeId:google.maps.MapTypeId.ROADMAP
+	};
+	var map=new google.maps.Map(document.getElementById("googleMap"), mapProp);
+	
+	var infowindow = new google.maps.InfoWindow({
+		content:"Hello World!"
+	});
+	
 	bus.stops(51.5072, 0.1275, function(data) {
 		console.log(data);
+		data.forEach(function(element) {
+			var marker=new google.maps.Marker({
+				position:new google.maps.LatLng(element.lat, element.lon)
+			});
+			marker.setMap(map);
+			google.maps.event.addListener(marker, element.name, function() {
+				infowindow.open(map, marker);
+			});
+		});
 	});
+	
 };
---
+++ labs/16 File System/README.md
@@ -0,0 +1,8 @@
+# NodeJS File System
+
+Uploading Base64 files via Postman and decoding them on the server, saving files to filesystem.
+
+Iterating through a folder on the server and creating image collections/objects. Displaying links to allow download and display.
+
+## Node Packages
+
