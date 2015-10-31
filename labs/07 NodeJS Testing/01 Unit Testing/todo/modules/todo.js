e2ed689df007cf9744fcff24eb07c900cc236f55
Mark Tyers <marktyers@gmail.com>
2015-10-31 17:07:11 +0000

    changed directory structure

---
+++ labs/07 NodeJS Testing/01 Unit Testing/todo/modules/todo.js
@@ -0,0 +1,43 @@
+
+storage = require('node-persist');
+storage.initSync();
+
+/* add a new item to the todo list. Notice that we are using the new 'Arrow Function' syntax from the ECMA6 specification. */
+exports.add = item => {
+	/* we check to see if the named item has already been added */
+	if (storage.getItem(item) === undefined) {
+		/* if it doesn't exist we add it with a quantity of '1' */
+		storage.setItem(item, {title: item, qty: 1});
+	} else {
+		/* if is already exists we retrive the current quantity and increment it before saving the new value. */
+		const current = storage.getItem(item).qty
+		storage.setItem(item, {title: item, qty: current+1})
+	}
+	return true
+};
+
+// calculates and returns the number of items in the list
+exports.count = () => {
+	return undefined
+};
+
+// empties the list10
+exports.clear = () => {
+	storage.clear()
+	return true
+};
+
+// returns an array containing all todo items
+exports.getAll = () => {
+	return storage.values()
+};
+
+// removes the item with the corresponding name
+exports.removeIndex = index => {
+
+}
+
+// removes item with the corresponding index.
+exports.removeItem = item => {
+
+}
