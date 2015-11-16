b52b7394689b1c589faa04fbeeba3f5b1e447707
Mark Tyers <marktyers@gmail.com>
2015-11-16 19:14:15 +0000

    completed AngularJS shopping example

--- labs/08 Introduction to AngularJS/01 Introduction/basic_math/js/shopping.js
+++ labs/08 Introduction to AngularJS/01 Introduction/basic_math/js/shopping.js
@@ -1,15 +1,22 @@
-// Creates a new module called 'calculatorApp'
-        angular.module('calculatorApp', []);
 
-        // Registers a controller to our module 'calculatorApp'.
-        angular.module('calculatorApp').controller('CalculatorController', function CalculatorController($scope) {
-          $scope.z = 0;
-          $scope.sum = function() {
-            $scope.z = $scope.x + $scope.y;
-          };
-        });
-
-        // load the app
-        angular.element(document).ready(function() {
-            angular.bootstrap(document, ['calculatorApp']);
-        });
+var app = angular.module('myApp', [])
+app.controller('shopController', function($scope) {
+  $scope.quantity = 1
+  $scope.price = 1.00
+  $scope.tax = 0.0
+  $scope.discount = 0.0
+  $scope.getTotal = function() {
+    console.log('getTotal()')
+    console.log($scope.quantity)
+    var total = $scope.quantity * $scope.price
+	  console.log("total before tax: " + total)
+    var tax = $scope.tax / 100
+	  tax = tax + 1
+    total = total * tax;
+	  console.log("total after tax: " + total)
+    total = total - $scope.discount
+	  console.log("total after discount: " + total)
+    $scope.total = total.toFixed(2)
+    console.log(" ")
+  }
+})
