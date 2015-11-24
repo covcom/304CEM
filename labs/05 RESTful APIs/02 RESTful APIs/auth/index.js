b042a40624a520e77ab03391a4039b69e65855ef
Mark Tyers <marktyers@gmail.com>
2015-11-24 21:06:54 +0000

    added comments to auth activity.

--- labs/05 RESTful APIs/02 RESTful APIs/auth/index.js
+++ labs/05 RESTful APIs/02 RESTful APIs/auth/index.js
@@ -6,18 +6,25 @@ server.use(restify.authorizationParser())
 
 const auth = require('./auth')
 
+/* POST is used to create new accounts. The url points to the 'accounts' collection. */
 server.post('/accounts', (req, res) => {
+  /* we pass the request object to an async function that takes a callback. The first callback parameter is an Error object or null if no errors are returned. The second paramter is the returned data. */
   auth.addAccount(req, (err, data) => {
+    /* we will return json data on success or failure */
     res.setHeader('content-type', 'application/json')
     if (err) {
+      /* if an error occurs respond with a suitable error code and message. */
       res.send(400, {status: 'error', message: err.message })
     } else {
+      /* success! so we return suitable data. */
       res.send(201, {status: 'success', message: 'account created', data: data})
     }
   })
 })
 
+/* This GET method is called on the 'accounts' collection and allows us to validate a username and password. */
 server.get('/accounts', (req, res) => {
+  /* we pass the request object to the function. It takes a second callback parameter which has two parameters, an error (or null) plus the returned data. */
   auth.getAccount(req, (err, data) => {
     res.setHeader('content-type', 'application/json')
     if (err) {
