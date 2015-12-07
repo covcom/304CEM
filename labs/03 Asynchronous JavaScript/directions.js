859d67bb655eda4d6b3593b288f39e6ae78a14d6
Mark Tyers <marktyers@gmail.com>
2015-12-07 22:07:15 +0000

    basic direction finder script

---
+++ labs/03 Asynchronous JavaScript/directions.js
@@ -0,0 +1,23 @@
+"use strict"
+
+const readline = require('readline-sync')
+const request = require('request')
+
+const origin = String(readline.question('start address: ')).trim()
+const destination = String(readline.question('finish address: ')).trim()
+
+const url = 'https://maps.googleapis.com/maps/api/directions/json?region=gb&origin='+origin+'&destination='+destination
+console.log(url)
+
+request.get(url, function(err, res, body) {
+		if (err) {
+			throw 'could not complete request'
+		} else {
+			//console.log(body)
+			const json = JSON.parse(body)
+			const route = json.routes[0].legs[0]
+			//console.log(route)
+			console.log(route.start_address)
+			console.log(route.end_address)
+		}
+	})
