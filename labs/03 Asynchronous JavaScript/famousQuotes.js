51ca25caf71fb05c2d6caece3834c9184f1199f9
Mark Tyers <marktyers@gmail.com>
2015-12-08 14:32:16 +0000

    fixed simple html scraper

--- labs/03 Asynchronous JavaScript/famousQuotes.js
+++ labs/03 Asynchronous JavaScript/famousQuotes.js
@@ -6,13 +6,16 @@ const cheerio = require('cheerio')
 
 const author = String(readline.question('author name: ')).trim()
 
-var page = 1
-const url = 'http://www.quotationspage.com/search.php3?startsearch=Search&homesearch='+author+'&page='+page
+const url = 'http://www.quotationspage.com/search.php3?startsearch=Search&homesearch='+author+'&page='+1
 console.log(url)
 
 request(url, function(err, res, body) {
 	try {
-		const $ = cheerio.load(body)
+		const $ = cheerio.load(body, {decodeEntities: false})
+		const quoteCount = $('small').text().match(/\d+/g).pop()
+		console.log('number of quotes: '+quoteCount)
+		const pages = Math.ceil(quoteCount / 10)
+		console.log('there are '+pages+' pages of quotes')
 		$('dt.quote > a').each(function(i, element) {
 			console.log(element.children[0].data)
 		})
@@ -20,5 +23,3 @@ request(url, function(err, res, body) {
 		
 	}
 })
-
-// http://www.smashingmagazine.com/2015/04/web-scraping-with-nodejs/
