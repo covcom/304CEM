6e2e26a7f4c356fa07430206db15016234c8c794
Mark Tyers <marktyers@gmail.com>
2016-07-19 06:43:08 +0000

    removed lab solution code

--- 03 Asynchronous JavaScript/currency.js
+++ 03 Asynchronous JavaScript/currency.js
@@ -3,26 +3,19 @@
 const request = require('request')
 
 try {
-	if (process.argv.length < 4) {
+	if (process.argv.length < 3) {
 		throw 'missing parameter'
 	}
-	let symbols = process.argv.slice(2)
-	const base = symbols.shift().toUpperCase()
-	symbols = symbols.join(',').toUpperCase()
+	const symbol = process.argv[2].toUpperCase()
 	const url = 'http://api.fixer.io/latest'
-	const querystring = {base: 'GBP', symbols: 'USD'}
+	const querystring = {symbols: symbol}
 	request.get({url: url, qs: querystring}, (err, res, body) => {
 		if (err) {
 			throw 'could not complete request'
 		}
 		const json = JSON.parse(body)
-		console.log(json)
-		console.log(json.rates)
-		symbols.split(',').forEach( element => {
-			console.log(element)
-			const rate = json.rates[element]
-			console.log(`1 ${json.base} = ${rate} ${element}`)
-		})
+		const output = JSON.stringify(json.rates, null, 2)
+		console.log(output)
 	})
 } catch(err) {
 	console.log(err)
