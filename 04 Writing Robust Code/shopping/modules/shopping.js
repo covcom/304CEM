7dae5703805df6d4505ddd9f2b37989ec0b82ca3
Mark Tyers <marktyers@gmail.com>
2016-07-20 10:11:41 +0800

    introduced formatting errors

--- 04 Writing Robust Code/shopping/modules/shopping.js
+++ 04 Writing Robust Code/shopping/modules/shopping.js
@@ -6,18 +6,22 @@
  */
 
 /** this global 'map' object will be used to store the items and quantities. It stores each item against a named key. */
-var data = new Map()
+var data = new Map();
 
 /** add a new item to the todo list. Notice that we are using the new 'Arrow Function' syntax from the ECMA6 specification. */
-exports.add = item => {
+exports.add = item  => {
+	debugger
 	/* check if the item is already in the list */
-	if (data.get(item) === undefined) {
+	if (data.get(item) == undefined)
+	{
 		/* if the item is not found it is added to the list with its quantity set to 1 */
 		data.set(item, {title: item, qty: 1})
-	} else {
+	} 
+	else 
+	{
 		/* the item is already in the list so it is retrieved and the quantity is incremented */
 		let i = data.get(item)
-		i.qty ++
+	    i.qty++
 		/* the new data is then stored in the map */
 		data.set(item, i)
 	}
@@ -26,7 +30,7 @@ exports.add = item => {
 
 
 /** calculates and returns the number of items in the list */
-exports.count = () => {
+exports.count = () => { 
 	/* the _Map_ object has a _size_ property that returns the number of items */
 	return data.size
 }
