361e0889370dbc6fef652ddc5acf62493c2c29f2
Mark Tyers <marktyers@gmail.com>
2016-08-03 21:56:21 +0100

    Finished function object lab

--- 02 ECMAScript 6 Classes/coffee/coffee.js
+++ 02 ECMAScript 6 Classes/coffee/coffee.js
@@ -1,28 +1,31 @@
-'use strict'
 
-class Coffee {
-
-	//const types = ['Arusha', 'Bergendal', 'Catuai', 'Kilimanjaro', 'Picchu']
-
-	constructor(roast, grams) {
-		this.roast = roast
-		this.grams = grams
+module.exports = function(roast, ounces = 8) { // can't use arrow functions here...
+	if (roast === undefined) {
+		throw new Error('missing roast type')
 	}
-
-	get size() {
-    if (this.grams < 200) {
+	this.roast = roast
+	this.ounces = ounces
+	this.getSize = () => {
+		if (this.ounces === 8) {
 			return 'small'
-		} else if (this.ounces < 500) {
+		} else if (this.ounces === 12) {
 			return 'medium'
-		} else {
+		} else if (this.ounces === 16) {
 			return 'large'
 		}
-  }
-
-  get order() {
-  	return `you ordered a ${this.size} ${this.roast} coffee.`
-  }
-
+	}
+	this.order = () => {
+		let msg
+		switch (this.getSize()) {
+		case 'small':
+		case 'medium':
+		case 'large':
+			msg = `You've ordered a ${this.getSize()} ${this.roast} coffee.`
+			break
+		default:
+			msg = `We don't have a ${this.roast} in that size!`
+			break
+		}
+		return msg
+	}
 }
-
-export default new Coffee
--- 02 ECMAScript 6 Classes/coffee/index.js
+++ 02 ECMAScript 6 Classes/coffee/index.js
@@ -1,6 +1,20 @@
 
 const Coffee = require('./coffee')
+try {
+	const coffee = new Coffee('House Blend', 12)
+	console.log(coffee.order())
 
-var coffee = new Coffee('Arusha', 300)
-console.log(coffee.order)
-console.log(coffee.roast)
+	const darkRoast = new Coffee('Dark Roast', 16)
+	console.log(darkRoast.order())
+
+	const specialBlend = new Coffee('Special Blend', 200)
+	console.log(specialBlend.order())
+
+	const kenyan = new Coffee('Kenyan')
+	console.log(kenyan.order())
+
+	const anon = new Coffee()
+	console.log(anon.order())
+} catch(err) {
+	console.log(`ERROR: ${err}`)
+}
