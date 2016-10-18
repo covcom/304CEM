3e6316fb4ae5d166748e22dfbdce71e0d44ad43d
marktyers <marktyers@gmail.com>
2016-10-18 10:19:33 -0400

    fixed async exercise

--- Lab Demos/week03/async_module/username.js
+++ Lab Demos/week03/async_module/username.js
@@ -2,11 +2,11 @@ exports.check = function (name, callback) {
     var names = ['colin', 'bob']
     if (names.indexOf(name) > -1) {
         // success
-        var result = "found"
+        var result = 'found'
         return callback(null, result)
     } else {
         // error
-        var message = "name not found"
+        var message = 'name not found'
         return callback(message)
     }
 }
