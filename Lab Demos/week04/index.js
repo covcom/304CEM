3a0a66f869c2d956969bc31982e2484631c303cc
Colin Stephen <ab5169@coventry.ac.uk>
2016-10-26 04:34:08 -0400

    include module for data storage and basic auth

--- Lab Demos/week04/index.js
+++ Lab Demos/week04/index.js
@@ -1,9 +1,11 @@
 'use strict'
 /* import the 'restify' module and create an instance. */
 const restify = require('restify')
+const movies = require('./movies')
 const server = restify.createServer()
 
-server.use(restify.bodyParser())
+server.use(restify.bodyParser())  // needed to access req.body (generally for POST, PUT, etc)
+server.use(restify.authorizationParser())  // needed to access req.authorization (for securing the API)
 
 const port = process.env.PORT || 8080
 
@@ -28,6 +30,15 @@ server.post('/users', function(req, res, next) {
   let body = req.body
   let message = `your data was: ${body}`
   res.send(200, message)
+  res.end()
+})
+
+// accept new movie titles for the DB
+server.post('/movies', function(req, res, next) {
+	const auth = req.authorization
+  let body = req.body
+	const data = movies.addNew(auth, body)
+  res.send(200, data)
   res.end()
 })
 
