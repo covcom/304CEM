ccc4a045e3aaeb796c1b26ce33c500534c245fa6
Mark Tyers <marktyers@gmail.com>
2016-10-31 20:16:09 +0000

    fixed linting errors

--- Lab Demos/week03/async_module/index.js
+++ Lab Demos/week03/async_module/index.js
@@ -1,16 +1,18 @@
 
+'use strict'
+
 const username = require('./username')
 // check username exists
-let uname = 'bob'
+const uname = 'bob'
 console.log(uname)
 username.check(uname, function(err, result) {
-  if (err) {
-    // it didn't work i.e. not found
-    console.error(err)
-    //throw new Error("broke looking for name")
-  } else {
-    // success: i.e. result has a value
-    console.log("It was found.")
-    console.log(result)
-  }
+	if (err) {
+		// it didn't work i.e. not found
+		console.error(err)
+		//throw new Error("broke looking for name")
+	} else {
+		// success: i.e. result has a value
+		console.log('It was found.')
+		console.log(result)
+	}
 })
