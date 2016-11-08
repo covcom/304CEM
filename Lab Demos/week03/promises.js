5b2f49591dbf51d854abf36c4fe676b05e9f4fe9
marktyers <marktyers@gmail.com>
2016-11-08 05:33:50 -0500

    simple promises demo

---
+++ Lab Demos/week03/promises.js
@@ -0,0 +1,41 @@
+
+'use strict'
+
+const request = require('request')
+
+try {
+	if (process.argv.length < 3) {
+		throw 'missing parameter'
+	}
+	const symbol = process.argv[2].toUpperCase()
+  getData(symbol).print.then( (result) => {
+    console.log(result)
+  }).catch( (error) => {
+    console.log(error)
+  })
+} catch(err) {
+	console.log(err)
+}
+
+function getData(symbol) {
+  return new Promise( (resolve, reject) => {
+    const url = `http://api.fixer.io/latest?symbols=${symbol}`
+    request.get( url, (err, res, body) => {
+		if (err) {
+			reject('could not complete request')
+		}
+		const json = JSON.parse(body)
+    resolve(json)
+	})
+  })
+}
+
+function print(data) {
+  return new Promise( (resolve, reject) => {
+    if (data.rates === undefined) {
+      reject('missing rates array')
+    }
+    console.log(data.rates)
+    resolve(data.rates)
+  })
+}
