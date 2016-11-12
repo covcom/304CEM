44e816e95ab1ff3569d485c1bb45627fa891dc47
Mark Tyers <marktyers@gmail.com>
2016-11-12 22:10:58 +0000

    search working

--- 08 Advanced APIs/bookshop/bookshop.js
+++ 08 Advanced APIs/bookshop/bookshop.js
@@ -9,8 +9,10 @@ exports.search = (request, callback) => {
 		console.log(query)
 		return google.searchByString(query)
 	}).then( data => {
-		console.log('search query complete')
+		console.log('tidying up array')
 		console.log(data)
+		return this.cleanArray(request, data)
+	}).then( data => {
 		callback(null, data)
 	}).catch( err => {
 		console.log('ERROR')
@@ -23,4 +25,16 @@ exports.extractParam = (request, param) => new Promise( (resolve, reject) => {
 	console.log(request.param)
 	if (request.params === undefined || request.params[param] === undefined) reject(new Error(`${param} parameter missing`))
 	resolve(request.params[param])
+})
+
+exports.cleanArray = (request, data) => new Promise((resolve) => {
+	const host = request.host || 'http://localhost'
+	console.log(data)
+	const clean = data.items.map(element => {
+		return {
+			title: element.volumeInfo.title,
+			link: `${host}/books/${element.id}`
+		}
+	})
+	resolve({books: clean})
 })
