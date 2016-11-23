85a73a992422530eb0878e89a5c5301c20aa70c7
Mark Tyers <marktyers@gmail.com>
2016-11-23 08:54:29 +0000

    fixed linter errors

--- Lab Demos/week08/modules/booksquery.js
+++ Lab Demos/week08/modules/booksquery.js
@@ -3,22 +3,25 @@
 const request = require('request')
 const googleapikey = require('../secrets').googleapi.key
 
-exports.doBookSearch = function doBookSearch (req, res, next) {
-  const q = req.query.q
-  const url = `https://www.googleapis.com/books/v1/volumes?q=${q}&key=${googleapikey}`
-  request.get(url, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      const books = []
-      for (let i = 0; i < 5; i++) {
-        let book = {
-          id: JSON.parse(body).items[i].id,
-          title: JSON.parse(body).items[i].volumeInfo.title,
-          authors: JSON.parse(body).items[i].volumeInfo.authors,
-          description: JSON.parse(body).items[i].volumeInfo.description
-        }
-        books.push(book)
-      }
-      res.send(books)
-    }
-  })
+exports.doBookSearch = function doBookSearch(req, res, next) {
+	const q = req.query.q
+	const url = `https://www.googleapis.com/books/v1/volumes?q=${q}&key=${googleapikey}`
+
+	request.get(url, function(error, response, body) {
+		if (!error && response.statusCode === 200) {
+			const books = []
+
+			for (let i = 0; i < 5; i++) {
+				const book = {
+					id: JSON.parse(body).items[i].id,
+					title: JSON.parse(body).items[i].volumeInfo.title,
+					authors: JSON.parse(body).items[i].volumeInfo.authors,
+					description: JSON.parse(body).items[i].volumeInfo.description
+				}
+
+				books.push(book)
+			}
+			res.send(books)
+		}
+	})
 }
--- Lab Demos/week08/modules/favourites.js
+++ Lab Demos/week08/modules/favourites.js
@@ -1,34 +1,37 @@
+'use strict'
 
 const storage = require('node-persist')
 
-exports.list = function list (req, res, next) {
-  const userstore = storage.create({dir: `./node-persist/${username}`})
-  userstore.init(err => {
-    if (err) throw new Error('Unable to initialise data store')
-    res.send(userstore.values())
-  })
+exports.list = function list(req, res, next) {
+	const userstore = storage.create({dir: `./node-persist/${username}`})
+
+	userstore.init(err => {
+		if (err) throw new Error('Unable to initialise data store')
+		res.send(userstore.values())
+	})
 }
 
-exports.add = function add (req, res, next) {
-  const userstore = storage.create({dir: `./node-persist/${username}`})
-  const book = JSON.parse(req.body)
-  userstore.init(err => {
-    if (err) throw new Error('Unable to initialise data store')
-    userstore.setItem(book.id, book, err => {
-      if (err) throw new Error(`Could not save book ${book}`)
-      res.send(book)
-    })
-  })
+exports.add = function add(req, res, next) {
+	const userstore = storage.create({dir: `./node-persist/${username}`})
+	const book = JSON.parse(req.body)
+
+	userstore.init(err => {
+		if (err) throw new Error('Unable to initialise data store')
+		userstore.setItem(book.id, book, err => {
+			if (err) throw new Error(`Could not save book ${book}`)
+			res.send(book)
+		})
+	})
 }
 
-exports.get = function get (req, res, next) {
-  
+exports.get = function get(req, res, next) {
+	
 }
 
-exports.update = function update (req, res, next) {
-  
+exports.update = function update(req, res, next) {
+	
 }
 
-exports.delete = function delete_ (req, res, next) {
-  
+exports.delete = function delete_(req, res, next) {
+	
 }
