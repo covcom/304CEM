6a766d79f79cbeb333a6ae10ec086b73fde940ec
Mark Tyers <marktyers@gmail.com>
2016-11-30 14:12:28 +0000

    added simple generator example

---
+++ 09 Advanced Async/generator.js
@@ -0,0 +1,37 @@
+'use strict'
+
+const request = require('request')
+
+/**
+ * wrapper to make sure the callback invokes the generator next() method
+ * @param {String} url - the url to callback
+ * @returns {undefined} - the yeild
+ */
+function makeAPICall(url) {
+	request.get( url, (err, res, body) => {
+		if (err) console.log(err)
+		it.next(JSON.parse(body))
+	})
+}
+
+/**
+ * the generator
+ * @param {String} symbol - the currency symbol to process
+ * @returns {undefined} - the yield
+ */
+function *main(symbol) {
+	try {
+		const url = `http://api.fixer.io/latest?symbols=${symbol}`
+		const result = yield makeAPICall(url)
+
+		console.log(JSON.stringify(result, null, 2))
+	} catch(err) {
+
+	}
+}
+
+const it = main('GBP')
+
+it.next()
+
+// https://davidwalsh.name/async-generators
