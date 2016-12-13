22ef5f0443c6eba25e2f347cbe18e37c2f0be0a9
Mark Tyers <marktyers@users.noreply.github.com>
2016-12-13 15:27:46 +0000

    fixed merge conflicts

--- Lab Demos/week05/mongo/lists-spec.js
+++ Lab Demos/week05/mongo/lists-spec.js
@@ -21,34 +21,13 @@ describe('shopping lists', () => {
 				if (err) expect(true).toBe(false) //error should not be thrown
 				schema.List.count({}, (err, count) => {
 					if (err) expect(true).toBe(false) //error should not be thrown
-=======
-const lists = require('./lists')
-const schema = require('./schema')
-
-describe('shopping list', () => {
-	beforeEach( done => {
-		schema.List.remove({}, err => {
-			if (err) expect(true).toBe(false)
-			new schema.List({name: 'colours', list: ['red', 'orange', 'yellow']}).save( (err, list) => {
-				if (err) expect(true).toBe(false)
-				schema.List.count({}, (err, count) => {
-					if (err) expect(true).toBe(false)
->>>>>>> eafc5135042d5bdc02c721a8073b441dfcac4eae
 					expect(count).toBe(1)
 					done()
 				})
 			})
 		})
 	})
-<<<<<<< HEAD
 
-	describe('add', () => {
-		it('should add a valid list', done => {
-			lists.add( {name: 'shopping', list: ['bread', 'butter', 'cheese']}, (err, data) => {
-				if (err) expect(true).toBe(false) //error should not be thrown
-				schema.List.count({}, (err, count) => {
-					if (err) expect(true).toBe(false) //error should not be thrown
-=======
 	describe('add', () => {
 		it('should add a valid list', done => {
 			const shopping = {
@@ -61,22 +40,13 @@ describe('shopping list', () => {
 				expect(data.name).toBe('shopping')
 				schema.List.count({}, (err, count) => {
 					if (err) expect(true).toBe(false)
->>>>>>> eafc5135042d5bdc02c721a8073b441dfcac4eae
 					expect(count).toBe(2)
 					done()
 				})
 			})
 		})
 	})
-<<<<<<< HEAD
 
-	describe('remove', () => {
-		it('should remove a named list', done => {
-			lists.remove('colours')
-			.then( () => {
-				schema.List.count({}, (err, count) => {
-					if (err) expect(true).toBe(false) //error should not be thrown
-=======
 	describe('count', () => {
 		it('should find one list', done => {
 			lists.count( (err, count) => {
@@ -91,22 +61,13 @@ describe('shopping list', () => {
 			lists.remove('colours').then( () => {
 				schema.List.count({}, (err, count) => {
 					if (err) expect(true).toBe(false)
->>>>>>> eafc5135042d5bdc02c721a8073b441dfcac4eae
 					expect(count).toBe(0)
 					done()
 				})
 			}).catch( err => {
-<<<<<<< HEAD
 				if (err) expect(true).toBe(false) //error should not be thrown
 			})
 		})
 	})
 
-=======
-				if (err) expect(true).toBe(false)
-				done()
-			})
-		})
-	})
->>>>>>> eafc5135042d5bdc02c721a8073b441dfcac4eae
 })
