cf5cac75a287ca36794df636bb49de99991949cb
Mark Tyers <marktyers@gmail.com>
2017-04-14 22:42:54 +0100

    removed linting errors

--- Lab Demos/week03/async_demo.js
+++ Lab Demos/week03/async_demo.js
@@ -1,33 +1,35 @@
 
-var async_func = function (name, callback) {
-    var names = ["colin", "bob"]
-    if (names.indexOf(name) > -1) {
-        // success
-        var result = "found"
-        return callback(null, result)
-    } else {
-        // error
-        var message = "name not found"
-        return callback(message)
-    }
+'use strict'
+
+const asyncFunc = function(name, callback) {
+	const names = ['colin', 'bob']
+	if (names.indexOf(name) > -1) {
+		// success
+		const result = 'found'
+		return callback(null, result)
+	} else {
+		// error
+		const message = 'name not found'
+		return callback(message)
+	}
 }
 
 // check username exists
-var check = function(name) {
-    async_func(name, function(err, result) {
-        if (err) {
-            // it didn't work i.e. not found
-            console.error(err)
-            throw new Error("broke looking for name")
-        } else {
-            // success: i.e. result has a value
-            console.log("It was found.")
-            console.log(result)
-        }
-    })
+const check = function(name) {
+	asyncFunc(name, function(err, result) {
+		if (err) {
+			// it didn't work i.e. not found
+			console.error(err)
+			throw new Error("broke looking for name")
+		} else {
+			// success: i.e. result has a value
+			console.log("It was found.")
+			console.log(result)
+		}
+	})
 }
 
-check("colin")
-check("nobody")
+check('colin')
+check('nobody')
 
 // function(err, data) {} is common callback signature
